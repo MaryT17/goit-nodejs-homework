12f315cafb3f16cf23ec9eabb5dc028cf17ceb90
MaryT17 <tmary11996@gmail.com>
2024-10-14 05:35:06 +0800

    update

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
 
-const contactsRouter = require('./routes/api/contacts')
+import { router as contactsRouter } from "./routes/api/contactsRouter.js";
+import { router as usersRouter } from "./routes/api/usersRouter.js"
 
-const app = express()
+const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
+// this is the JSON parser middleware
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+//tells Express to serve static files frm the public directory
+// http://localhost:3000/avatar/medium.webp
+// we need to acess the localhost port followed by the directory of the static file and the file name and extension
+app.use(express.static("public"));
 
-app.use((req, res) => {
+// initialize the base path for the contacts router
+app.use('/api/contacts', contactsRouter);
+app.use("/api/users", usersRouter);
+
+// error handling using res.status()
+// not found
+app.use((_req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
-app.use((err, req, res, next) => {
+// server error
+app.use((err, _req, res, _next) => {
   res.status(500).json({ message: err.message })
 })
 
-module.exports = app
+export { app };
--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+import { app } from "./app.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+mongoose.connect(DB_HOST, {
+
+})
+.then(() => {
+  app.listen(3000, () => {
+    console.log("Server is running. Use our API on port: 3000");
+  });
+  
+  console.log("Database connect successful");
+
+})
+.catch((error) => {
+  console.log(`Server not running. Error message: ${error.message}`);
+  process.exit(1); // this terminates the mongoose connection
 });
